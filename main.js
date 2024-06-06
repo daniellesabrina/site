fab738a76d018834a6e3bd679451b6f1610e7976
daniellesabrina <116652741+daniellesabrina@users.noreply.github.com>
2024-06-05 21:30:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
-const caixaPrincipal = document.querySelector(‘.caixa-
-principal’);
+const caixaPrincipal = document.querySelector(“.caixa-
+principal”);
 
-const caixaPerguntas = document.querySelector(‘.caixa-
-perguntas’);
+const caixaPerguntas = document.querySelector(“.caixa-
+perguntas”);
 
-const caixaAlternativas = document.querySelector(‘.caixa-
-alternativas’);
+const caixaAlternativas = document.querySelector(“.caixa-
+alternativas”);
 
-const caixaResultado = document.querySelector(‘.caixa-
-resultado’);
+const caixaResultado = document.querySelector(“.caixa-
+resultado”);
 
-const textoResultado = document.querySelector(‘.texto-
-resultado’);
+const textoResultado = document.querySelector(“.texto-
+resultado”);
+
 const perguntas = [
 {
 enunciado: “Pergunta 1”,
-alternativas: [
-“Alternativa 1”,
-“Alternativa 2”
-]
-}
-]
-const perguntas = [ //abre lista de perguntas
-{ //abre objeto de pergunta
-enunciado: “Pergunta 1”,
-alternativas: [
-“Alternativa 1”,
-“Alternativa 2”
-]
-} //fecha objeto de pergunta
-] //fecha lista de perguntas
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
+{
+enunciado: “Pergunta 2”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
+];
