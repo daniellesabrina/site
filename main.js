43a7c16f94d53ddd305a1fea8620c0612c0e68b9
daniellesabrina <116652741+daniellesabrina@users.noreply.github.com>
2024-06-05 21:24:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,70 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
+const perguntas = [
+{
+enunciado: “Pergunta 1”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
 
+{
+enunciado: “Pergunta 2”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
+<script src=”script.js”></script>
+const caixaPrincipal = document.querySelector(‘.caixa-
+principal’);
+const caixaPrincipal = document.querySelector(‘.caixa-
+principal’);
 
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j=0; j<botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
+const caixaPerguntas = document.querySelector(‘.caixa-
+perguntas’);
+
+const caixaAlternativas = document.querySelector(‘.caixa-
+alternativas’);
+
+const caixaResultado = document.querySelector(‘.caixa-
+resultado’);
+
+const textoResultado = document.querySelector(‘.texto-
+resultado’);
+const lista = [item1, item2]
+const perguntas = []
+const lapis = {
+tamanho: 20,
+tipo: ‘HB’,
+cor: ‘Grafite’,
+temBorrachaAtras: false
 }
-
-
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-19T23:59:59");
- 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+const perguntas = [
+{
+enunciado: “Pergunta 1”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
 }
+]
+const perguntas = [ //abre lista de perguntas
+{ //abre objeto de pergunta
+enunciado: “Pergunta 1”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
+} //fecha objeto de pergunta
+] //fecha lista de perguntas
+const perguntas = [
+{
+enunciado: “Pergunta 1”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
+},
 
-
-
-
+{
+enunciado: “Pergunta 2”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
+}
+]
