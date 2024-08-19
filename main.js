20188e0a39cf09ba59d40bf01630b80f066c2a76
daniellesabrina <116652741+daniellesabrina@users.noreply.github.com>
2024-08-19 19:36:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,25 @@
-:root{
-    --cor-fundo: #01080E;
-    --cor-principal: #0B0D20 ;
-    --cor-secundaria: #212333;
-    --cor-destaque: #2BDEFD;
-    --cor-texto: #D7F9FF;
-}
+const caixaPrincipal = document.querySelector(“.caixa-
+principal”);
 
-body{
-    background-color: var(--cor-fundo);
-    color: var(--cor-texto);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-}
+const caixaPerguntas = document.querySelector(“.caixa-
+perguntas”);
 
-.caixa-principal{
-    background-color: var(--cor-principal);
-    width: 90%;
-    max-width: 600px;
-    text-align: center;
-    padding: 20px;
-}
+const caixaAlternativas = document.querySelector(“.caixa-
+alternativas”);
 
-h1{
-    color: var(--cor-destaque);
-}
+const caixaResultado = document.querySelector(“.caixa-
+resultado”);
 
-.caixa-perguntas{
-    padding-bottom: 10px;
-}
+const textoResultado = document.querySelector(“.texto-
+resultado”);
 
-button{
-    background-color: var(--cor-secundaria);
-    color: var(--cor-texto);
-    border: none;
-    border-radius: 15px;
-    padding: 15px;
-    transition: background-color 0.3s;
-}
-
-button:hover{
-    background-color: var(--cor-destaque);
-    color: var(--cor-principal);
-}
+const perguntas = [
+{
+enunciado: “Pergunta 1”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
+{
+enunciado: “Pergunta 2”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
+];
